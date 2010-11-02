2f1afe5d3be5e8759b11f9544380251188332c3e
Will Sobel <will@systeminsights.com>
2010-11-02 12:19:11 -0700

    Delphi project updates.

--- delphi/Adapter.dpr
+++ delphi/Adapter.dpr
@@ -3,60 +3,11 @@ program DepphiAdapter;
 {$APPTYPE CONSOLE}
 
 uses
-  DelphiAdapter, Windows, SysUtils;
+  MTConnect, Windows, SysUtils;
 var
-  Adapter: TAdapter;
-  XPosition: TSample;
-  ProgramName: TEvent;
-  Exec: TExecution;
-  Mode: TControllerMode;
-  Avail: TAvailability;
-  OverTemp: TCondition;
-
-  i: Integer;
+  Connector: TMTConnector;
 
 begin
-  Adapter := NewAdapter(7878);
-  XPosition := NewSample('Xpos');
-  Adapter.addDatum(XPosition);
-  Exec := NewExecution('exec');
-  Adapter.addDatum(Exec);
-  Avail := NewAvailability('avail');
-  Adapter.addDatum(Avail);
-  ProgramName := NewEvent('program');
-  Adapter.addDatum(ProgramName);
-  OverTemp := NewCondition('over_temp');
-  Adapter.addDatum(OverTemp);
-  Mode := NewControllerMode('mode');
-  Adapter.addDatum(Mode);
-  Adapter.startServerThread;
-
-  ProgramName.setValue('prog.iso');
-  Mode.setValue(ControllerMode_MANUAL);
-
-  for i := 1 to 120 Do
-  Begin
-    Writeln(i);
-    Adapter.timestamp;
-    XPosition.setValue(i);
-
-    if (i = 5)
-    Then Mode.setValue(ControllerMode_AUTOMATIC);
-
-    if ((i mod 4) = 0)
-    Then Exec.setValue(ExecutionState_READY)
-    else Exec.setValue(ExecutionState_ACTIVE);
-
-    if (i = 10)
-    Then OverTemp.setValue(Condition_FAULT, 'Overtemp Fault', '1123', 'HIGH', '');
-
-    if (i = 12)
-    Then OverTemp.setValue(Condition_NORMAL, '', '', '', '');
-
-    Adapter.sendChangedData;
-    Windows.Sleep(1000);
-  end;
-
-  Adapter.unavailable;
-  Adapter.stopServer;
+  Connector := TMTConnector.Create;
+  Windows.Sleep(10000);
 end.
