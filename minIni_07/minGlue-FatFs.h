a0262dc95335fe53bdb6e94ef669282e2de0361a
Matt Powley <matt.powley@renishaw.com>
2018-04-23 12:21:01 +0100

    C++11 Conversion: Updated to the latest version of minIni

--- minIni_07/minGlue-FatFs.h
+++ minIni_07/minGlue-FatFs.h
@@ -1,19 +1,12 @@
-/*  Glue functions for the minIni library, based on the FatFs and Tiny-FatFs
+/*  Glue functions for the minIni library, based on the FatFs and Petit-FatFs
  *  libraries, see http://elm-chan.org/fsw/ff/00index_e.html
  *
- *  Copyright (c) ITB CompuPhase, 2008-2009
+ *  By CompuPhase, 2008-2012
+ *  This "glue file" is in the public domain. It is distributed without
+ *  warranties or conditions of any kind, either express or implied.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
- *  use this file except in compliance with the License. You may obtain a copy
- *  of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations
- *  under the License.
+ *  (The FatFs and Petit-FatFs libraries are copyright by ChaN and licensed at
+ *  its own terms.)
  */
 
 #define INI_BUFFERSIZE  256       /* maximum line length, maximum path length */
@@ -22,13 +15,23 @@
  * to enable the "string functions" fgets() and fputs().
  */
 #include "ff.h"                   /* include tff.h for Tiny-FatFs */
-#define INI_FILETYPE    FIL
 
-#define ini_openread(filename,file)   (f_open((file),(filename),FA_READ+FA_OPEN_EXISTING) == 0)
-#define ini_openwrite(filename,file)  (f_open((file),(filename),FA_WRITE+FA_CREATE_ALWAYS) == 0)
-#define ini_close(file)               f_close(file)
-#define ini_read(buffer,size,file)    fgets((buffer),(size),(file))
-#define ini_write(buffer,file)        fputs((buffer),(file))
-#define ini_rename(source,dest)       f_rename((source),(dest))
-#define ini_remove(filename)          f_unlink(filename)
-#define ini_rewind(file)              f_lseek((file),0)
+#define INI_FILETYPE    FIL
+#define ini_openread(filename,file)   (f_open((file), (filename), FA_READ+FA_OPEN_EXISTING) == FR_OK)
+#define ini_openwrite(filename,file)  (f_open((file), (filename), FA_WRITE+FA_CREATE_ALWAYS) == FR_OK)
+#define ini_close(file)               (f_close(file) == FR_OK)
+#define ini_read(buffer,size,file)    f_gets((buffer), (size),(file))
+#define ini_write(buffer,file)        f_puts((buffer), (file))
+#define ini_remove(filename)          (f_unlink(filename) == FR_OK)
+
+#define INI_FILEPOS                   DWORD
+#define ini_tell(file,pos)            (*(pos) = f_tell((file)))
+#define ini_seek(file,pos)            (f_lseek((file), *(pos)) == FR_OK)
+
+static int ini_rename(TCHAR *source, const TCHAR *dest)
+{
+  /* Function f_rename() does not allow drive letters in the destination file */
+  char *drive = strchr(dest, ':');
+  drive = (drive == NULL) ? dest : drive + 1;
+  return (f_rename(source, drive) == FR_OK);
+}
